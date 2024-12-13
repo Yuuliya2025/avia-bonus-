618faf3abcc21a0b6fc37bda91d8431ccf95fd9b
Texnolog24 <latkina0808@yandex.ru>
2024-12-13 18:36:46 +0500

    Добавлено содержимое папки проекта

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        int ticketPrice = 10253;
+
+        int rublesPerMile = 20;
+
+        int bonusMiles = ticketPrice / rublesPerMile;
+
+        System.out.println("Количество бонусных миль: " + bonusMiles + " руб.");
+    }
+}
+
